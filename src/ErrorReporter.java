af59b961d7081af7beef4a9ebcb3d3fd24180a5c
‘windssind’ <1179610806@qq.com>
2024-04-22 14:59:14 +0800

    fix some bugs

--- src/ErrorReporter.java
+++ src/ErrorReporter.java
@@ -22,22 +22,22 @@ public class ErrorReporter {
         switch (type)
         {
             case RedefinedFunc:
-                System.err.println("Error type 4 at Line " + lineNO + " :Redefined function: " + name + ".");
+                System.err.println("Error type 4 at Line " + lineNO + ": Redefined function: " + name + ".");
                 break;
             case RedefinedVar:
-                System.err.println("Error type 3 at Line " + lineNO + " :Redefined variable: " + name + ".");
+                System.err.println("Error type 3 at Line " + lineNO + ": Redefined variable: " + name + ".");
                 break;
             case UndefinedVar:
-                System.err.println("Error type 1 at Line " + lineNO + " :Undefined variable: " + name + ".");
+                System.err.println("Error type 1 at Line " + lineNO + ": Undefined variable: " + name + ".");
                 break;
             case UndefinedFunc:
                 System.err.println("Error type 2 at Line " + lineNO + ": Undefined function: " + name + ".");
                 break;
             case NotAFunction:
-                System.err.println("Error type 10 at Line " + lineNO + " :Not a function: " + name + ".");
+                System.err.println("Error type 10 at Line " + lineNO + ": Not a function: " + name + ".");
                 break;
             case FuncParamFalse:
-                System.err.println("Error type 8 at Line " + lineNO  + " :Function is not applicable for arguments.");
+                System.err.println("Error type 8 at Line " + lineNO  + ": Function is not applicable for arguments.");
                 break;
             case ValAssignNotLegal:
                 System.err.println("Error type 5 at Line " + lineNO + ": Type mismatched for assignment.");
@@ -49,7 +49,7 @@ public class ErrorReporter {
                 System.err.println("Error type 9 at Line " + lineNO + "Not an array: " + name);
                 break;
             case NotAnArray:
-                System.err.println("Error type 9 at Line " + lineNO + " :Not an array: " + name);
+                System.err.println("Error type 9 at Line " + lineNO + ": Not an array: " + name);
                 break;
             case LeftHandSideAFunc:
                 System.err.println("Error type 11 at Line " + lineNO + ": The left-hand side of an assignment must be a variable.");
